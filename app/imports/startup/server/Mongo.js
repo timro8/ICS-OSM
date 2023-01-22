a079ce0a325373f972cbe73d404db88846f3df9a
susankpma <susanm@hawaii.edu>
2023-01-21 14:24:07 -1000

    created default room data and insert rooms collection is empty
    Mongo.js - import RoomCollection and initialize
    
    settings.development.json - default room data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,11 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/StuffCollection';
+import { Rooms } from '../../api/room/RoomCollection';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
 function addData(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.define(data);
+  Rooms.define(data);
 }
 
 // Initialize the StuffsCollection if empty.
@@ -13,5 +15,13 @@ if (Stuffs.count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+// Initialize the RoomsCollection if empty.
+if (Rooms.count() === 0) {
+  if (Meteor.settings.defaultRoomData) {
+    console.log('Creating default room data.');
+    Meteor.settings.defaultRoomData.map(data => addData(data));
   }
 }
