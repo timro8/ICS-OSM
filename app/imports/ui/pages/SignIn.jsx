ef63c2f77135aff09e22a6b07c072cbb367b135a
graquepo <graquepo@hawaii.edu>
2023-01-21 23:39:38 -1000

    update sign-in, sign-up pages, and footer

--- app/imports/ui/pages/SignIn.jsx
+++ app/imports/ui/pages/SignIn.jsx
@@ -7,6 +7,7 @@ import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+import { EnvelopeFill, KeyFill } from 'react-bootstrap-icons';
 
 /**
  * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
@@ -39,27 +40,47 @@ const SignIn = () => {
   // console.log('render', error, redirect);
   // if correct authentication, redirect to page instead of login screen
   if (redirect) {
-    return (<Navigate to="/" />);
+    return (<Navigate to="/home" />);
   }
+
+  document.title = 'OSMICS - Sign In';
   // Otherwise return the Login form.
   return (
     <Container id={PAGE_IDS.SIGN_IN} className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
-          <Col className="text-center">
-            <h2>Login to your account</h2>
-          </Col>
           <AutoForm schema={bridge} onSubmit={data => submit(data)}>
             <Card>
-              <Card.Body>
-                <TextField id={COMPONENT_IDS.SIGN_IN_FORM_EMAIL} name="email" placeholder="E-mail address" />
-                <TextField id={COMPONENT_IDS.SIGN_IN_FORM_PASSWORD} name="password" placeholder="Password" type="password" />
+              <Card.Body height="50%">
+                <Col className="text-center">
+                  <h2>Login to your account</h2>
+                </Col>
+                <Row className="mt-4">
+                  <Col className="col-1 mt-1 ms-1">
+                    <EnvelopeFill style={{ fontSize: '25px', color: 'lightskyblue' }} />
+                  </Col>
+                  <Col className="mx-2">
+                    <TextField id={COMPONENT_IDS.SIGN_IN_FORM_EMAIL} name="email" placeholder="Email" label="" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="col-1 mt-1 ms-1">
+                    <KeyFill style={{ fontSize: '25px', color: 'gold' }} />
+                  </Col>
+                  <Col className="mx-2">
+                    <TextField id={COMPONENT_IDS.SIGN_IN_FORM_PASSWORD} name="password" placeholder="Password" type="password" label="" />
+                  </Col>
+                </Row>
                 <ErrorsField />
-                <SubmitField id={COMPONENT_IDS.SIGN_IN_FORM_SUBMIT} />
+                <Col className="d-flex justify-content-center">
+                  <SubmitField id={COMPONENT_IDS.SIGN_IN_FORM_SUBMIT} />
+                </Col>
+
               </Card.Body>
             </Card>
           </AutoForm>
           <Alert variant="secondary">
+            Not a member?&nbsp;
             <Link to="/signup">Click here to Register</Link>
           </Alert>
           {error === '' ? (
--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -10,6 +10,7 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 import { UserProfiles } from '../../api/user/UserProfileCollection';
 import { defineMethod } from '../../api/base/BaseCollection.methods';
+import { EnvelopeFill, KeyFill } from 'react-bootstrap-icons';
 
 /**
  * SignUp component is similar to signin component, but we create a new user instead.
@@ -56,18 +57,32 @@ const SignUp = () => {
     <Container id={PAGE_IDS.SIGN_UP} className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
-          <Col className="text-center">
-            <h2>Register your account</h2>
-          </Col>
           <AutoForm schema={bridge} onSubmit={data => submit(data)}>
             <Card>
               <Card.Body>
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_FIRST_NAME} name="firstName" placeholder="First name" />
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_LAST_NAME} name="lastName" placeholder="Last name" />
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_EMAIL} name="email" placeholder="E-mail address" />
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_PASSWORD} name="password" placeholder="Password" type="password" />
+                <Col className="text-center">
+                  <h2>Sign Up</h2>
+                </Col>
+                <Row>
+                  <Col className="col-1 mt-1 ms-1">
+                    <EnvelopeFill style={{ fontSize: '25px', color: 'lightskyblue' }} />
+                  </Col>
+                  <Col>
+                    <TextField id={COMPONENT_IDS.SIGN_UP_FORM_EMAIL} name="email" placeholder="E-mail address" label="" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="col-1 mt-1 ms-1">
+                    <KeyFill style={{ fontSize: '25px', color: 'gold' }} />
+                  </Col>
+                  <Col>
+                    <TextField id={COMPONENT_IDS.SIGN_UP_FORM_PASSWORD} name="password" placeholder="Password" type="password" label="" />
+                  </Col>
+                </Row>
                 <ErrorsField />
-                <SubmitField id={COMPONENT_IDS.SIGN_UP_FORM_SUBMIT} />
+                <Col className="d-flex justify-content-center">
+                  <SubmitField id={COMPONENT_IDS.SIGN_UP_FORM_SUBMIT} />
+                </Col>
               </Card.Body>
             </Card>
           </AutoForm>
