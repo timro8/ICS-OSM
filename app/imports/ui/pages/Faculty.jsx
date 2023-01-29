6e407b9ab21c03aae133d68760783bf887c7b824
malialiu <mliu0141@gmail.com>
2023-01-28 17:59:47 -1000

    Updated Faculty.jsx

--- app/imports/ui/pages/Faculty.jsx
+++ app/imports/ui/pages/Faculty.jsx
@@ -1,47 +1,62 @@
 import React, { useState } from 'react';
-import {Button, Card, Container, Form, Modal, Stack} from 'react-bootstrap';
+import {Button, Card, Container, Form, Modal, Row, Stack} from 'react-bootstrap';
 import {Search} from "react-bootstrap-icons";
 
 /* A simple static component to render some text for the landing page. */
 const Faculty = () => {
     // pop up window: https://react-bootstrap.github.io/components/modal/
-    // <a href="/addFaculty" className="btn btn-primary" role="button">Add Faculty</a>
+    // show pop up to add faculty
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    // show pop up to show faculty detail information
     const [showDetail, setShowDetail] = useState(false);
     const handleCloseDetail = () => setShowDetail(false);
     const handleShowDetail = () => setShowDetail(true);
+
+    function cards(room, name, office_hours){
+        return (
+                <Card className="m-2" style={{fontWeight: "bold", width: "250px"}} onClick={handleShowDetail}>
+                    <Card.Body>
+                        <Card.Text>{room}</Card.Text>
+                        <Card.Text>{name}</Card.Text>
+                        <Card.Text>{office_hours}</Card.Text>
+                        <Button className="faculty-card-button">Edit</Button>
+                    </Card.Body>
+                </Card>
+        );
+    }
 
     return (
         <>
         <Container className="py-3">
             <Container className="faculty-page">
-                <Form className="search-bar">
-                    <Form.Control placeholder="Search..."/>
-                    <Button><Search/></Button>
-                </Form>
-                <Button variant="primary" onClick={handleShow}>Add Faculty</Button>
-                <Stack>
-                    <Card className="faculty-card" onClick={handleShowDetail}>
-                        <Card.Body>
-                            <Card.Text>307</Card.Text>
-                            <Card.Text>Cam Moore</Card.Text>
-                            <Button>Edit</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card className="faculty-card">
-                        <Card.Body>
-                            <Card.Text>309 - Person 1, 2, 3</Card.Text>
-                            <Card.Text>Phone #</Card.Text>
-                            <Card.Text>Office Hours</Card.Text>
-                            <Button>Edit</Button>
-                        </Card.Body>
-                    </Card>
-                </Stack>
+
+                <Container className="d-flex align-items-center justify-content-center">
+                    <Form className="search-input">
+                        <Form.Control placeholder="Search..."/>
+                        <Button><Search/></Button>
+                    </Form>
+                </Container>
+
+                <Button style={{marginLeft: "1vw", marginBottom: "10px"}} variant="primary" onClick={handleShow}>Add Faculty</Button>
+
+                <Container className="d-flex align-items-center justify-content-center">
+                    <Row>
+                        {cards(307, "Cam Moore", "Office Hours")}
+                        {cards(307, "Cam Moore", "Office Hours")}
+                        {cards(307, "Cam Moore", "Office Hours")}
+                        {cards(307, "Cam Moore", "Office Hours")}
+                        {cards(309, "Person 1, 2, 3", "Office Hours")}
+                        {cards(309, "Person 1, 2, 3", "Office Hours")}
+                        {cards(309, "Person 1, 2, 3", "Office Hours")}
+                    </Row>
+                </Container>
             </Container>
         </Container>
+
+            {/* Add Faculty Form */}
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Add Faculty</Modal.Title>
@@ -87,16 +102,16 @@ const Faculty = () => {
                 </Modal.Footer>
             </Modal>
 
+            {/* Show detail information of faculty */}
             <Modal show={showDetail} onHide={handleCloseDetail}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Room #</Modal.Title>
-                </Modal.Header>
+                <Modal.Header closeButton/>
                 <Modal.Body>
+                    Room # <br/>
                     Name <br/>
                     Phone number <br/>
-                    email <br/>
-                    office hours <br/>
-                    role
+                    Email <br/>
+                    Office hours <br/>
+                    Role
                 </Modal.Body>
             </Modal>
         </>
