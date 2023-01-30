7ba4a734ffb131eac8f3c90a59ab9b70c2fe3ed3
malialiu <mliu0141@gmail.com>
2023-01-30 11:37:05 -1000

    Add comments and move faculty card in component

--- app/imports/ui/components/FacultyDetail.jsx
+++ app/imports/ui/components/FacultyDetail.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 const FacultyDetail = props => {
 
-    if (!props.show) {
-        return null
-    }
-    // pop up window: https://react-bootstrap.github.io/components/modal/
+  // eslint-disable-next-line react/prop-types
+  const { show, onClose } = props;
+  if (!show) {
+    return null;
+  }
+  // pop up window: https://react-bootstrap.github.io/components/modal/
 
-    return (
-        <Modal show={props.show}>
-            <Modal.Header closeButton onClick={props.onClose}/>
-            <Modal.Body>
-                Room # <br/>
-                Name <br/>
-                Phone number <br/>
-                Email <br/>
-                Office hours <br/>
-                Role
-            </Modal.Body>
-        </Modal>
-    );
-}
+  return (
+    <Modal show={show}>
+      <Modal.Header closeButton onClick={onClose} />
+      <Modal.Body>
+        Room # <br />
+        Name <br />
+        Phone number <br />
+        Email <br />
+        Office hours <br />
+        Role
+      </Modal.Body>
+    </Modal>
+  );
+};
 
+FacultyDetail.propTypes = {
+  props: PropTypes.shape({
+    show: PropTypes.bool,
+    onClose: PropTypes.func,
+  }).isRequired,
+};
 
 export default FacultyDetail;
