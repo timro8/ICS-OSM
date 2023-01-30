000235279f67aff013dfcb49133939e05a0c5d7f
graquepo <graquepo@hawaii.edu>
2023-01-30 12:10:24 -1000

    update profile page to use faculty collection data

--- app/imports/ui/pages/ProfileTemp.jsx
+++ app/imports/ui/pages/ProfileTemp.jsx
@@ -1,38 +1,42 @@
 import React from 'react';
 import { Col, Row, Card, Container, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
 import { PAGE_IDS } from '../utilities/PageIDs';
-import { ROLE } from '../../api/role/Role';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { Profiles } from '../../api/faculty/FacultyCollection';
+import { FacultyRoles } from '../../api/faculty/FacultyRolesCollection';
 
-/** TODO
- *Get real user data from collections.
- */
-const ProfileTemp = () => (
+// TODO: Get real user data from collections.
+
+const MakeProfile = ({ profile, position }) => (
   <Container id={PAGE_IDS.PROFILE} className="py-3" fluid>
     <Row>
       <Col className="d-flex justify-content-center">
-        <Image id="imgProfile" roundedCircle src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" width="300px" />
+        <Image id="imgProfile" roundedCircle src={profile.image} width="300px" />
       </Col>
     </Row>
     <Card id="cardProfile">
       <Col className="text-center pt-3">
-        <h1>John Doe</h1>
-        <p>john@foo.com</p>
+        <h1>{profile.firstName} {profile.lastName}</h1>
+        <p>{profile.email}</p>
         <hr />
         <span className="small">About Me:</span>
-        <p>I like ICS 414</p>
+        <p>{profile.bio}</p>
         <hr />
         <span className="small">Role:</span>
-        <p className="fw-bold">{ROLE.USER}</p>
+        <p className="fw-bold">{position.role}</p>
         <hr />
         <span className="small">Room Number:</span>
-        <p className="fw-bold">Post 303B</p>
+        <p className="fw-bold">{profile.room}</p>
         <hr />
         <span className="small">Phone Number</span>
-        <p className="fw-bold">(808)123-4567</p>
+        <p className="fw-bold">{profile.phoneNumber}</p>
         <hr />
         <span className="small">Office Hours:</span>
-        <p className="fw-bold">Monday 1pm-2pm</p>
+        <p className="fw-bold">{profile.officeHours}</p>
       </Col>
     </Card>
     <Row>
@@ -43,4 +47,39 @@ const ProfileTemp = () => (
   </Container>
 );
 
-export default ProfileTemp;
+const Profile = () => {
+  const { ready, profiles } = useTracker(() => {
+    const subscription = Meteor.subscribe(Profiles.userPublicationName);
+    const subscription2 = FacultyRoles.subscribeFacultyRole();
+    const rdy = subscription.ready();
+    const rdy2 = subscription2.ready();
+    const profileItems = Profiles.find({}, { sort: { lastName: 1 } }).fetch();
+    return {
+      profiles: profileItems,
+      ready: rdy, rdy2,
+    };
+  }, []);
+  return (ready ? (
+    <Container id={PAGE_IDS.PROFILE} className="py-3">
+      <Row>
+        {profiles.map((profile) => <MakeProfile key={profile._id} profile={profile} />)}
+      </Row>
+    </Container>
+  ) : <LoadingSpinner message="Loading Profile" />);
+};
+
+MakeProfile.propTypes = {
+  profile: PropTypes.shape({
+    image: PropTypes.string,
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    officeHours: PropTypes.string,
+    bio: PropTypes.number,
+    email: PropTypes.string,
+  }).isRequired,
+  position: PropTypes.shape({
+    role: PropTypes.string,
+  }).isRequired,
+};
+
+export default Profile;
