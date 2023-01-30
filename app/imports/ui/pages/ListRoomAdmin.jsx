3f64297a15069f10ec665cd27ac08870672c1174
susankpma <susanm@hawaii.edu>
2023-01-30 11:31:24 -1000

    updated list room admin
    Checklist: React-01

--- app/imports/ui/pages/ListRoomAdmin.jsx
+++ app/imports/ui/pages/ListRoomAdmin.jsx
@@ -1,36 +1,10 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Container, Card, Image, Row, Col, Button } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import { Container, Row } from 'react-bootstrap';
 import { Rooms } from '../../api/room/RoomCollection';
+import RoomItem from '../components/RoomItem';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { PAGE_IDS } from '../utilities/PageIDs';
-
-const MakeCard = ({ room }) => (
-  <Col>
-    <Card border="primary" className="h-100">
-      <Card.Header>
-        <Image src={room.picture} alt={`${room.roomNumber} picture`} width={75} />
-        <Card.Title>{room.roomNumber}</Card.Title>
-        <Card.Subtitle><span className="date">{room.location}</span></Card.Subtitle>
-      </Card.Header>
-      <Card.Body>
-        <Card.Text>
-          <strong>Status:</strong> {room.status}
-        </Card.Text>
-        <Card.Text>
-          <strong>Room capacity:</strong> {room.capacity}
-        </Card.Text>
-        <Card.Text>
-          <strong>Owner:</strong> {room.owner}
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="primary">Edit Room</Button>
-      </Card.Footer>
-    </Card>
-  </Col>
-);
 
 /* Renders a table containing all of the Room documents. Use <RoomItemAdmin> to render each row. */
 const ListRoomAdmin = () => {
@@ -40,7 +14,7 @@ const ListRoomAdmin = () => {
     const subscription = Rooms.subscribeRoomAdmin();
     // Determine if the subscription is ready
     const rdy = subscription.ready();
-    // Get the Stuff documents
+    // Get the Room documents
     const items = Rooms.find({}).fetch();
     return {
       rooms: items,
@@ -50,22 +24,10 @@ const ListRoomAdmin = () => {
   return ready ? (
     <Container id={PAGE_IDS.LIST_ROOM_ADMIN} className="py-3">
       <Row xs={1} md={2} lg={4} className="g-2">
-        {rooms.map((room) => <MakeCard key={room._id} room={room} />)}
+        {rooms.map((room) => <RoomItem key={room._id} room={room} />)}
       </Row>
     </Container>
   ) : <LoadingSpinner />;
-};
-
-MakeCard.propTypes = {
-  room: PropTypes.shape({
-    roomNumber: PropTypes.string,
-    location: PropTypes.string,
-    status: PropTypes.string,
-    capacity: PropTypes.number,
-    _id: PropTypes.string,
-    owner: PropTypes.string,
-    picture: PropTypes.string,
-  }).isRequired,
 };
 
 export default ListRoomAdmin;
