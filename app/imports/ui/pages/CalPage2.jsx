45d9174f76b44af7b0a7ff44eaa31c2b25b84cd2
Giorgio <tran.giorgio@gmail.com>
2023-01-31 19:09:06 -1000

    Implement timeline via FullCalendar

--- app/imports/ui/pages/CalPage2.jsx
+++ app/imports/ui/pages/CalPage2.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
+// import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { Container } from 'react-bootstrap';
 
 const CalPage2 = () => (
-  <FullCalendar
-    plugins={[dayGridPlugin]}
-    initialView="dayGridMonth"
-  />
+  <Container className="py-3">
+    <FullCalendar
+      plugins={[timeGridPlugin]}
+      initialView="timeGridWeek"
+      allDaySlot={false}
+    />
+  </Container>
 );
 
 export default CalPage2;
