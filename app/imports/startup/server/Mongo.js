203a42216e56366146574f0a6866f7ac192445ca
graquepo <graquepo@hawaii.edu>
2023-02-01 10:57:24 -1000

    updated mongo.js for faculty data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/StuffCollection';
 import { Rooms } from '../../api/room/RoomCollection';
+import { FacultyProfiles } from '../../api/faculty/FacultyCollection';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -12,6 +13,11 @@ function addData(data) {
 function addRoomData(data) {
   console.log(`  Adding: ${data.roomNumber} (${data.owner})`);
   Rooms.define(data);
+}
+
+function addFacultyData(data) {
+  console.log(`  Adding: ${data.email} (${data.email})`);
+  FacultyProfiles.define(data);
 }
 
 // Initialize the StuffsCollection if empty.
@@ -27,5 +33,13 @@ if (Rooms.count() === 0) {
   if (Meteor.settings.defaultRoomData) {
     console.log('Creating default room data.');
     Meteor.settings.defaultRoomData.map(data => addRoomData(data));
+  }
+}
+
+// Initialize the RoomsCollection if empty.
+if (FacultyProfiles.count() === 0) {
+  if (Meteor.settings.defaultFacultyData) {
+    console.log('Creating default faculty data.');
+    Meteor.settings.defaultFacultyData.map(data => addFacultyData(data));
   }
 }
