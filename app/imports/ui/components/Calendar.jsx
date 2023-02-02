696e7187c8e2a239a774ad9375d7c3ed02b6b558
Giorgio <tran.giorgio@gmail.com>
2023-02-01 15:53:50 -1000

    Change headerToolbar and add reserve button

--- app/imports/ui/components/Calendar.jsx
+++ app/imports/ui/components/Calendar.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 const Calendar = () => (
   <div className="w-100">
+    <h2>Conference Room 302</h2>
     <FullCalendar
       plugins={[timeGridPlugin]}
       initialView="timeGridWeek"
@@ -11,6 +12,19 @@ const Calendar = () => (
       slotMinTime="07:00:00"
       slotMaxTime="18:00:00"
       height="auto"
+      headerToolbar={{
+        start: 'title',
+        center: '',
+        end: 'myCustomButton prev,next',
+      }}
+      customButtons={{
+        myCustomButton: {
+          text: 'Reserve Room',
+          click: function () {
+            alert('clicked the custom button!');
+          },
+        },
+      }}
     />
   </div>
 );
