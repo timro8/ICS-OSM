ad09dcfdf7b0510e2f27e7ff1c1d76b94ee19d56
Giorgio <tran.giorgio@gmail.com>
2023-02-03 16:13:35 -1000

    Update MATPCollection and add events to cal

--- app/imports/api/matp/MATPCollections.js
+++ app/imports/api/matp/MATPCollections.js
@@ -3,6 +3,7 @@ import { Stuffs } from '../stuff/StuffCollection';
 import { Rooms } from '../room/RoomCollection';
 import { AdminProfiles } from '../user/AdminProfileCollection';
 import { UserProfiles } from '../user/UserProfileCollection';
+import { Events302 } from '../events/Events302Collection';
 
 class MATPClass {
   collections;
@@ -18,6 +19,7 @@ class MATPClass {
       Stuffs,
       UserProfiles,
       Rooms,
+      Events302,
     ];
     /*
      * A list of collection class instances in the order required for them to be sequentially loaded from a file.
@@ -27,6 +29,7 @@ class MATPClass {
       UserProfiles,
       Stuffs,
       Rooms,
+      Events302,
     ];
 
     /*
--- app/imports/ui/components/calendar/Calendar.jsx
+++ app/imports/ui/components/calendar/Calendar.jsx
@@ -1,14 +1,29 @@
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import React, { useState } from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
 import RoomResModal from './RoomResModal';
+import { Events302 } from '../../../api/events/Events302Collection';
 
 const Calendar = () => {
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
+
+  const { ready, events } = useTracker(() => {
+    const subscription = Events302.subscribeEvents302();
+    const rdy = subscription.ready();
+    const allEvents = Events302.find({}, {}).fetch();
+    console.log('allEvents', allEvents);
+    return {
+      events: allEvents,
+      ready: rdy,
+    };
+  });
+  console.log(events);
   return (
     <div className="w-100">
+      { ready ? console.log('events', events) : console.log('is ready', ready) }
       <h2>Conference Room 302</h2>
       <FullCalendar
         plugins={[timeGridPlugin]}
