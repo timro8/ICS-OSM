1561c4f2f49fdf936226d8821467534136096e8e
Giorgio <tran.giorgio@gmail.com>
2023-02-07 22:53:13 -1000

    Fix eslint and remove signup button

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+import { BoxArrowRight, PersonFill } from 'react-bootstrap-icons';
 import { ROLE } from '../../api/role/Role';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -12,22 +12,17 @@ const Landing = () => {
   }
   return (
     <Row id={PAGE_IDS.LANDING} className="py-3 landingpage align-content-center text-center">
-      {/* TODO: Remove empty col */}
-      <Col>
-        <h1></h1>
-      </Col>
       <Col>
         <h2 className="landingtitle">WELCOME TO ICS-OSM!</h2>
         {/* TODO: Combine paragraphs into single div/p/span/etc. */}
         <p className="landingtext">This is an app that allows you to see the 3rd Floor of POST </p>
         <p className="landingtext">as well as some rooms in the Keller building</p>
         <Col direction="horizontal" className="marginland">
-          <Button variant="success" size="lg" href="/signuprequest" className="landingbut ">Sign Up</Button>
-          <Button variant="secondary" size="lg" href="/signin">Sign In</Button>
+          <Button variant="success" size="lg" href="/signin">Sign In</Button>
         </Col>
       </Col>
     </Row>
   );
-}
+};
 
 export default Landing;
