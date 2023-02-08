299e27932b9d4ddb6aa6caddab1d6d14ee17b7aa
James Louie Grande <jlgrande@hawaii.edu>
2023-02-08 09:37:49 -1000

    Add other info to home page

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -4,27 +4,60 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 
 const Landing = () => (
   <Container id={PAGE_IDS.HOME} className="py-3">
-    <h1>Hi, Good Morning</h1>
+    <h1>Hi (user), Good Morning</h1>
     <Row>
+      <h2>Total</h2>
+      <ProgressBar>
+        <ProgressBar variant="success" now={83} key={1} />
+        <ProgressBar variant="warning" now={14} key={2} />
+        <ProgressBar variant="danger" now={3} key={3} />
+      </ProgressBar>
+    </Row>
+    <Row>
+      <h2>Pacific Ocean Science and Technology</h2>
       <Card style={{ width: '20rem' }}>
         <Card.Body>
           <Card.Title>Rooms Occupied</Card.Title>
           <Card.Text style={{ fontSize: '3rem' }}>45</Card.Text>
-          <ProgressBar now={70} />
+          <ProgressBar now={45} />
         </Card.Body>
       </Card>
       <Card style={{ width: '20rem' }}>
         <Card.Body>
           <Card.Title>Rooms Vacant</Card.Title>
-          <Card.Text style={{ fontSize: '3rem' }}>4</Card.Text>
-          <ProgressBar now={20} />
+          <Card.Text style={{ fontSize: '3rem' }}>9</Card.Text>
+          <ProgressBar variant="info" now={9} />
         </Card.Body>
       </Card>
       <Card style={{ width: '20rem' }}>
         <Card.Body>
           <Card.Title>Rooms Out of Commission</Card.Title>
           <Card.Text style={{ fontSize: '3rem' }}>2</Card.Text>
-          <ProgressBar variant='danger' now={10} />
+          <ProgressBar variant='danger' now={2} />
+        </Card.Body>
+      </Card>
+    </Row>
+    <Row>
+      <h2>Keller Hall</h2>
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <Card.Title>Rooms Occupied</Card.Title>
+          <Card.Text style={{ fontSize: '3rem' }}>42</Card.Text>
+          <ProgressBar now={42} />
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <Card.Title>Rooms Vacant</Card.Title>
+          <Card.Text style={{ fontSize: '3rem' }}>3</Card.Text>
+          <ProgressBar now={3} />
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <Card.Title>Rooms Out of Commission</Card.Title>
+          <Card.Text style={{ fontSize: '3rem' }}>0</Card.Text>
+          <ProgressBar variant='danger' now={0} />
         </Card.Body>
       </Card>
     </Row>
