3b21fabb5adb0ee101e536833f594b24aaed8393
James Louie Grande <jlgrande@hawaii.edu>
2023-02-08 09:13:12 -1000

    Add content to home page (mockup)

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Card, Container, ProgressBar, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
-/* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Row id={PAGE_IDS.HOME} className="py-3 align-content-center text-center">
-    <Col>
-      <h2>WELCOME</h2>
-      <h3>Instructions</h3>
-    </Col>
-  </Row>
+  <Container id={PAGE_IDS.HOME} className="py-3">
+    <h1>Hi, Good Morning</h1>
+    <Row>
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <Card.Title>Rooms Occupied</Card.Title>
+          <Card.Text style={{ fontSize: '3rem' }}>70%</Card.Text>
+          <ProgressBar now={70} />
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <Card.Title>Rooms Vacant</Card.Title>
+          <Card.Text style={{ fontSize: '3rem' }}>20%</Card.Text>
+          <ProgressBar now={20} />
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <Card.Title>Rooms Out of Commission</Card.Title>
+          <Card.Text style={{ fontSize: '3rem' }}>10%</Card.Text>
+          <ProgressBar now={10} />
+        </Card.Body>
+      </Card>
+    </Row>
+    <Row>
+      <Card style={{ width: '30rem' }}>
+        <Card.Header>Equipments</Card.Header>
+        <Card.Body>
+          <Card.Text>Total Computers: 150</Card.Text>
+          <Card.Text>Total Network Jacks: 300</Card.Text>
+          <Card.Text>Total Phone Jacks: 300</Card.Text>
+        </Card.Body>
+      </Card>
+    </Row>
+  </Container>
 );
 
 export default Landing;
