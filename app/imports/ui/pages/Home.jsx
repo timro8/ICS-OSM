8a70e2e8f2b7358e1a8848fe3351f40aad4af8fb
James Louie Grande <jlgrande@hawaii.edu>
2023-02-10 00:00:03 -1000

    Add map to home page and icons (mockup)

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -7,7 +7,39 @@ const Landing = () => (
   <Row id={PAGE_IDS.HOME} className="py-3 align-content-center text-center">
     <Col>
       <h2>WELCOME</h2>
-      <h3>Instructions</h3>
+      <svg xlinkHref="/images/post-3rd-floor.svg" />
+      <div style={{ backgroundImage: 'url(\'/images/post-3rd-floor.svg\')', height: '900px', width: '1100px', position: 'relative' }} />
+      <div
+        className="map-icon"
+        style={{
+          top: '500px',
+          left: '120px',
+          backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2019/05/johnson-300x300.jpeg\')',
+        }}
+      />
+      <div
+        className="map-icon"
+        style={{
+          top: '450px',
+          left: '120px',
+          backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/cam-moore.jpg\')',
+        }}
+      />
+      <div
+        className="map-icon"
+        style={{
+          top: '450px',
+          left: '135px',
+          backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2019/05/johnson-300x300.jpeg\')',
+        }}
+      />
+      <div
+        className="map-icon"
+        style={{
+          top: '450px',
+          left: '200px',
+        }}
+      />
     </Col>
   </Row>
 );
