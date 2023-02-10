af83fcda744da9a0bdcd69e9035d6d3c2b889325
James Louie Grande <jlgrande@hawaii.edu>
2023-02-10 10:48:06 -1000

    Add subscription

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -1,48 +1,70 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import { Rooms } from '../../api/room/RoomCollection';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+// TODO: Use FacultyRoomCollection to determine who's in which room
+// - Map through each facultyroom and make it return an element
+
+// TODO: Associate the room with the positioning of the elements (maybe use the room #)
 
 /* A simple static component to render some text for the landing page. */
-const Landing = () => (
-  <Row id={PAGE_IDS.HOME} className="py-3 align-content-center text-center">
-    <Col>
-      <h2>WELCOME</h2>
-      <svg xlinkHref="/images/post-3rd-floor.svg" />
-      <div style={{ backgroundImage: 'url(\'/images/post-3rd-floor.svg\')', height: '900px', width: '1100px', position: 'relative' }}>
-        <div
-          className="map-icon"
-          style={{
-            top: '200px',
-            left: '120px',
-            backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2019/05/johnson-300x300.jpeg\')',
-          }}
-        />
-        <div
-          className="map-icon"
-          style={{
-            top: '150px',
-            left: '120px',
-            backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/cam-moore.jpg\')',
-          }}
-        />
-        <div
-          className="map-icon"
-          style={{
-            top: '150px',
-            left: '135px',
-            backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2019/05/johnson-300x300.jpeg\')',
-          }}
-        />
-        <div
-          className="map-icon"
-          style={{
-            top: '150px',
-            left: '200px',
-          }}
-        />
-      </div>
-    </Col>
-  </Row>
-);
+const Landing = () => {
+  const { ready, rooms } = useTracker(() => {
+    const subscription = Rooms.subscribeRoom();
+    return {
+      ready: subscription.ready(),
+      rooms: Rooms.find().fetch(),
+    };
+  });
+
+  return ready ? (
+    <Row id={PAGE_IDS.HOME} className="py-3 align-content-center text-center">
+      <Col>
+        <h2>WELCOME</h2>
+        {rooms.map(room => (
+          <div key={room._id}>
+            Room Number: {room.roomNumber}
+          </div>
+        ))}
+        <div style={{ backgroundImage: 'url(\'/images/post-3rd-floor.svg\')', height: '900px', width: '1100px', position: 'relative' }}>
+          <div
+            className="map-icon"
+            style={{
+              top: '200px',
+              left: '120px',
+              backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2019/05/johnson-300x300.jpeg\')',
+            }}
+          />
+          <div
+            className="map-icon"
+            style={{
+              top: '150px',
+              left: '120px',
+              backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/cam-moore.jpg\')',
+            }}
+          />
+          <div
+            className="map-icon"
+            style={{
+              top: '150px',
+              left: '135px',
+              backgroundImage: 'url(\'https://www.ics.hawaii.edu/wp-content/uploads/2019/05/johnson-300x300.jpeg\')',
+            }}
+          />
+          <div
+            className="map-icon"
+            style={{
+              top: '150px',
+              left: '200px',
+            }}
+          />
+        </div>
+      </Col>
+    </Row>
+  ) : <LoadingSpinner message="Loading Room" />;
+};
 
 export default Landing;
