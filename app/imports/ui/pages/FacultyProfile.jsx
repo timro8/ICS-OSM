a4b99e6c03269b55533e4075b2936d536ac86f80
graquepo <graquepo@hawaii.edu>
2023-02-09 23:40:32 -1000

    edit pop up page

--- app/imports/ui/pages/FacultyProfile.jsx
+++ app/imports/ui/pages/FacultyProfile.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Col, Row, Card, Container, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
 import { useTracker } from 'meteor/react-meteor-data';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { Faculties } from '../../api/faculty/FacultyCollection';
 import LoadingSpinner from '../components/LoadingSpinner';
+import EditFacultyProfile from './EditFacultyProfile';
 
 // TODO: Get real user data from collections.
 
@@ -45,11 +45,7 @@ const FacultyProfile = () => {
           <p className="fw-bold">{faculty[0].officeHours}</p>
         </Col>
       </Card>
-      <Row>
-        <Col className="d-flex justify-content-center py-3">
-          <Link to={`/editfacultyprofile/${_id}`} className="btn btn-primary">Edit Profile</Link>
-        </Col>
-      </Row>
+      <EditFacultyProfile />
     </Container>
   ) : <LoadingSpinner message="Loading Faculty Profile" />);
 };
