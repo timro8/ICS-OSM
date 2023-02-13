36f5b94fa9fd243e2ef75c91355fa69d5e523247
Giorgio <tran.giorgio@gmail.com>
2023-02-13 10:57:55 -1000

    Make navbar and footer hidden in '/' and '/signin'

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Container, Col } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => {
   const divStyle = { paddingTop: '15px' };
-  return (
-    <footer className="bg-light footer mt-auto py-3 pt-1">
-      <Container style={divStyle}>
-        <Col className="text-center small">
-          OSMICS <br />
-          University of Hawaii<br />
-          <a href="https://ics414t3.github.io/">https://ics414t3.github.io</a>
-        </Col>
-      </Container>
-    </footer>
+
+  const location = useLocation();
+
+  return (!(location.pathname === '/' || location.pathname === '/signin') ?
+    (
+      <footer className="bg-light footer mt-auto py-3 pt-1">
+        <Container style={divStyle}>
+          <Col className="text-center small">
+            OSMICS <br />
+            University of Hawaii<br />
+            <a href="https://ics414t3.github.io/">https://ics414t3.github.io</a>
+          </Col>
+        </Container>
+      </footer>
+    ) : ''
   );
 };
 
