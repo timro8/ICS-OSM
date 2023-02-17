9706ea85d49a75150553d44f57f7b643e47c301b
Giorgio <tran.giorgio@gmail.com>
2023-02-17 00:23:45 -1000

    Make landing page prettier

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row, Button, Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { Navigate } from 'react-router-dom';
 import { PAGE_IDS } from '../utilities/PageIDs';
@@ -18,18 +18,18 @@ const Landing = () => {
       id={PAGE_IDS.LANDING}
       className="py-3 d-flex align-items-center text-center min-vh-100"
       style={{
-        background: '#004730',
+        background: 'radial-gradient(circle at 10% 80%, rgb(220, 217, 249) 10%, rgb(172, 224, 217) 89.8%)',
       }}
     >
       <Container className="h-100 d-flex flex-column gap-5 justify-content-center text-white">
-        <h1 className="display-1">WELCOME TO OSMICS!</h1>
-        <h2 className="display-6">An application designed to improved your experience in the ICS department.</h2>
+        <h1 className="display-1 fw-bold">WELCOME TO OSMICS!</h1>
+        <h2 className="display-6 fw-bold">Designed to improve your experience in the ICS department.</h2>
         <div>
           <Button
-            variant="dark"
+            variant="outline-light"
             size="lg"
             href="/signin"
-            style={{ width: '200px', padding: '20px' }}
+            style={{ width: '200px', padding: '20px', border: '4px solid #F8F9FA', fontWeight: 'bold' }}
           >
             Sign In
           </Button>
