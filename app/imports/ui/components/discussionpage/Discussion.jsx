91ed78acb172b8d73ca203fb69b6e1de1c0a26af
JeromeWasserman <46459313+JeromeWasserman@users.noreply.github.com>
2023-02-19 17:39:48 -1000

    for now

--- app/imports/ui/components/discussionpage/Discussion.jsx
+++ app/imports/ui/components/discussionpage/Discussion.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Badge, Image } from 'react-bootstrap';
+import { Card, Badge, Row, Col } from 'react-bootstrap';
 
 /** Renders a single row of Faculty members in a (Admin) table. See pages/AdminPageFacultyComponent.jsx. */
 const Discussions = ({ discussion }) => (
   <Card className="py-sm-3 px-2 mb-2">
-    <div className="px-3">
-      <Card.Subtitle> Posted by: {discussion.firstName} {discussion.lastName}</Card.Subtitle>
-      <Card.Title>{discussion.name}</Card.Title>
-      <Badge pill bg="dark" style={{ width: '10rem' }}>
-        {discussion.flair}
-      </Badge>
-    </div>
-    <Card.Body>{discussion.description}</Card.Body>
-    <Card.Body><Image src={discussion.image} fluid /></Card.Body>
+    <Row className="px-3">
+      <Col>
+        <Card.Img src={discussion.image} height="200rem" />
+      </Col>
+      <Col>
+        <Card.Subtitle> Posted by: {discussion.owner}</Card.Subtitle>
+        <Card.Title>{discussion.title}</Card.Title>
+        <Badge pill bg="dark" style={{ width: '10rem' }}>
+          {discussion.flair}
+        </Badge>
+        <Card.Text className="text">{discussion.description}</Card.Text>
+      </Col>
+    </Row>
   </Card>
 );
 
@@ -21,9 +25,7 @@ const Discussions = ({ discussion }) => (
 Discussions.propTypes = {
   discussion: PropTypes.shape({
     image: PropTypes.string,
-    name: PropTypes.string,
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
+    title: PropTypes.string,
     owner: PropTypes.string,
     description: PropTypes.string,
     flair: PropTypes.string,
