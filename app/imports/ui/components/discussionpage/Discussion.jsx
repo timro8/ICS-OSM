a4719fdee39a96ce5f5c3ed347bce4d37a9ab85f
JeromeWasserman <46459313+JeromeWasserman@users.noreply.github.com>
2023-02-21 16:22:33 -1000

    added links to userprofiles but it seems to not work also added a view button

--- app/imports/ui/components/discussionpage/Discussion.jsx
+++ app/imports/ui/components/discussionpage/Discussion.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Badge, Stack, Image } from 'react-bootstrap';
+import { Card, Badge, Stack, Image, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row of Faculty members in a (Admin) table. See pages/AdminPageFacultyComponent.jsx. */
 const Discussions = ({ discussion }) => (
   <Card className="py-sm-3 px-2 mb-3">
     <Stack direction="horizontal" gap={3}>
       {/* eslint-disable-next-line jsx-a11y/alt-text */}
-      <div><Image className="image" src={discussion.image} height="150rem" width="200rem" /></div>
+      <div><Image className="image" src={discussion.image} height="200rem" width="200rem" thumbnail /></div>
       <Stack direction="vertical">
-        <Card.Subtitle> Posted by: {discussion.owner}</Card.Subtitle>
+        <Card.Subtitle> Posted by: <Link key="creator" to={`/profile/:id/${discussion.owner}`}>{discussion.owner} </Link> </Card.Subtitle>
         <Card.Title>{discussion.title}</Card.Title>
         <Badge pill bg="dark" style={{ width: '10rem' }}>
           {discussion.flair}
         </Badge>
-        <Card.Text className="text">{discussion.description}</Card.Text>
+        <Card.Text className="text" style={{ height: '5rem' }}>{discussion.description}</Card.Text>
+        <Button style={{ width: '10rem' }}>View</Button>
       </Stack>
     </Stack>
   </Card>
@@ -28,7 +30,7 @@ Discussions.propTypes = {
     owner: PropTypes.string,
     description: PropTypes.string,
     flair: PropTypes.string,
-    id: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
 
