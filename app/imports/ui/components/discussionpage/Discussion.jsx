84151fae8833efe9a5abadfcbdb880b4cb86b9f2
JeromeWasserman <46459313+JeromeWasserman@users.noreply.github.com>
2023-02-21 15:48:09 -1000

    so far

--- app/imports/ui/components/discussionpage/Discussion.jsx
+++ app/imports/ui/components/discussionpage/Discussion.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Badge, Row, Col } from 'react-bootstrap';
+import { Card, Badge, Stack, Image } from 'react-bootstrap';
 
 /** Renders a single row of Faculty members in a (Admin) table. See pages/AdminPageFacultyComponent.jsx. */
 const Discussions = ({ discussion }) => (
-  <Card className="py-sm-3 px-2 mb-2">
-    <Row className="px-3">
-      <Col>
-        <Card.Img src={discussion.image} height="200rem" />
-      </Col>
-      <Col>
+  <Card className="py-sm-3 px-2 mb-3">
+    <Stack direction="horizontal" gap={3}>
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <div><Image className="image" src={discussion.image} /></div>
+      <Stack direction="vertical">
         <Card.Subtitle> Posted by: {discussion.owner}</Card.Subtitle>
         <Card.Title>{discussion.title}</Card.Title>
         <Badge pill bg="dark" style={{ width: '10rem' }}>
           {discussion.flair}
         </Badge>
         <Card.Text className="text">{discussion.description}</Card.Text>
-      </Col>
-    </Row>
+      </Stack>
+    </Stack>
   </Card>
 );
 
