9e5a908926995b06dc6f39973a3358768b0412f3
susankpma <susanm@hawaii.edu>
2023-02-22 20:59:38 -1000

    update room collection and add room

--- app/imports/ui/components/AddRoom.jsx
+++ app/imports/ui/components/AddRoom.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
-import { useTracker } from 'meteor/react-meteor-data';
 import { Button, Modal } from 'react-bootstrap';
 import { AutoForm, ErrorsField, SubmitField, TextField, SelectField, NumField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { Rooms } from '../../api/room/RoomCollection';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
-import { FacultyProfiles } from '../../api/user/FacultyProfileCollection';
-import { defineMethod, updateMethod } from '../../api/base/BaseCollection.methods';
-import LoadingSpinner from './LoadingSpinner';
+import { defineMethod } from '../../api/base/BaseCollection.methods';
 
 // form schema based on the Room collection
 const bridge = new SimpleSchema2Bridge(Rooms._schema);
@@ -20,56 +17,21 @@ const AddRoom = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const { faculty, ready } = useTracker(() => {
-    const subscription = FacultyProfiles.subscribeFacultyProfileAdmin();
-    const rdy = subscription.ready();
-    const facultyList = FacultyProfiles.find({}, { sort: { lastName: 1, firstName: 1 } }).fetch();
-    return {
-      faculty: facultyList,
-      ready: rdy,
-    };
-  });
-
-  const facOccupants = [];
-  facOccupants.push({ label: '---', value: '---' });
-  faculty.map((fac) => facOccupants.push(({ label: `${fac.firstName} ${fac.lastName}`, value: `${fac.email}` })));
-
   // data added to the Room collection. If there are errors, an error message will appear. If the data is submitted successfully, a success message will appear. Upon success, the form will reset for the user to add additional rooms.
   const submit = (data, formRef) => {
-    const { roomKey, roomNumber, location, status, capacity, roomSqFoot, roomClassification, occupants, picture } = data;
-    let collectionName = Rooms.getCollectionName();
-    const definitionData = { roomKey, roomNumber, location, status, capacity, roomSqFoot, roomClassification, occupants, picture };
+    const { roomKey, roomNumber, location, status, capacity, roomSqFoot, roomClassification, picture } = data;
+    const collectionName = Rooms.getCollectionName();
+    const definitionData = { roomKey, roomNumber, location, status, capacity, roomSqFoot, roomClassification, picture };
 
     defineMethod.callPromise({ collectionName, definitionData })
       .catch(error => swal('Error', error.message, 'error'))
       .then(() => {
         swal('Success', 'Room added successfully', 'success');
+        formRef.reset();
       });
-
-    collectionName = FacultyProfiles.getCollectionName();
-    occupants.forEach((o) => {
-      const facultyId = FacultyProfiles.findByEmail(o)._id;
-      const email = FacultyProfiles.findByEmail(o).email;
-      const password = FacultyProfiles.findByEmail(o).password;
-      const firstName = FacultyProfiles.findByEmail(o).firstName;
-      const lastName = FacultyProfiles.findByEmail(o).lastName;
-      const facRole = FacultyProfiles.findByEmail(o).facRole;
-      const image = FacultyProfiles.findByEmail(o).image;
-      const bio = FacultyProfiles.findByEmail(o).bio;
-      let rooms = [];
-      const phoneNumber = FacultyProfiles.findByEmail(o).phoneNumber;
-      const officeHours = FacultyProfiles.findByEmail(o).officeHours;
-      rooms = FacultyProfiles.findByEmail(o).rooms;
-      rooms.push(roomNumber);
-
-      const updateData = { id: facultyId, email, password, firstName, lastName, facRole, image, bio, rooms, phoneNumber, officeHours };
-      updateMethod.callPromise({ collectionName, updateData });
-    });
-    formRef.reset();
   };
   let fRef = null;
-  return ready ? (
+  return (
     <>
       <Button id={COMPONENT_IDS.ADD_ROOM} variant="primary" onClick={handleShow}>
         Add Room
@@ -98,7 +60,6 @@ const AddRoom = () => {
               name="roomClassification"
               allowedValues={['Office', 'Sink', 'Conference', 'Cubicle', 'ICS Library', 'ASECOLAB', 'Mail', 'Main Office', 'Lab', 'ICSpace', 'Storage', 'ICS IT', 'OFCSVC', 'LNG']}
             />
-            <SelectField name="occupants" options={facOccupants} />
             <TextField name="picture" />
             <SubmitField value="submit" />
             <ErrorsField />
@@ -111,6 +72,6 @@ const AddRoom = () => {
         </Modal.Footer>
       </Modal>
     </>
-  ) : <LoadingSpinner />;
+  );
 };
 export default AddRoom;
