c7f7ed3ed824c5a9c343e3a4be6fa4e893161b3d
susankpma <susanm@hawaii.edu>
2023-03-01 10:59:09 -1000

    update list admin

--- app/imports/ui/pages/ListRoomAdmin.jsx
+++ app/imports/ui/pages/ListRoomAdmin.jsx
@@ -51,7 +51,7 @@ const ListRoomAdmin = () => {
   return ready ? (
     <Container id={PAGE_IDS.LIST_ROOM_ADMIN} className="py-3">
       <Row xs={1} md={2} lg={4} className="g-2">
-        {roomData.map((room) => <RoomItem key={room.roomKey} room={room} />)}
+        {roomData.map((room) => <RoomItem key={room._id} room={room} />)}
       </Row>
     </Container>
   ) : <LoadingSpinner />;
