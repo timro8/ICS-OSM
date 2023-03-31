f0b1c4685674aa30491f200bc21e143c9382023b
James Louie Grande <jlgrande@hawaii.edu>
2023-03-30 18:37:07 -1000

    Convert ReservationsTable as a section for home

--- app/imports/ui/pages/home/Home.jsx
+++ app/imports/ui/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../../utilities/PageIDs';
 import FacultySection from './section/faculty/FacultySection';
-import ReservationsTable from '../../components/ReservationsTable';
+import ReservationsSection from './section/reservation/ReservationsSection';
 import { OccupiedProgressBar, VacantProgressBar, OutOfCommissionProgressBar } from './progress-bar/ProgressBar';
 import PostThirdFloor from './section/map/POST-third-floor/postThirdFloor';
 
@@ -16,7 +16,7 @@ const Home = () => (
     <PostThirdFloor />
     <FacultySection />
     <Row className="simple-card">
-      <ReservationsTable />
+      <ReservationsSection />
     </Row>
   </Container>
 );
