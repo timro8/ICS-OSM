462b66c4a0450ba846a380a125fbf6f963cf52b3
James Louie Grande <jlgrande@hawaii.edu>
2023-03-31 18:06:23 -1000

    Refactor and create message if list is empty

--- app/imports/ui/pages/home/section/map/POST-third-floor/list-view/ListView.jsx
+++ app/imports/ui/pages/home/section/map/POST-third-floor/list-view/ListView.jsx
@@ -5,68 +5,77 @@ import AccordionHeader from 'react-bootstrap/AccordionHeader';
 import AccordionBody from 'react-bootstrap/AccordionBody';
 import PropTypes from 'prop-types';
 
-const ListView = ({ rooms }) => {
-  const getOccupiedRooms = () => rooms.filter(room => room.status === 'Occupied');
-  const getVacantRooms = () => rooms.filter(room => room.status === 'Vacant');
-  const getOutOfCommissionRooms = () => rooms.filter(room => room.status === 'Out of Commission');
+const RoomsList = ({ rooms, status }) => {
+  const roomsByStatus = () => rooms.filter(room => room.status === status);
+  if (!roomsByStatus().length) {
+    return (<div style={{ color: 'darkgray' }}>There are no rooms {status.toLowerCase()}</div>);
+  }
+  return roomsByStatus().map(room => <ul><li key={rooms._id}>{room.roomNumber}</li></ul>);
+};
 
-  const OccupiedRooms = () => (getOccupiedRooms().map(room => <ul><li>{room.roomNumber}</li></ul>));
-  const VacantRooms = () => (getVacantRooms().map(room => <ul><li>{room.roomNumber}</li></ul>));
-  const OutOfCommissionRooms = () => (getOutOfCommissionRooms().map(room => <ul><li>{room.roomNumber}</li></ul>));
+const ListView = ({ rooms }) => (
+  <>
+    <h2 style={{ margin: '15px 0' }}>List View</h2>
+    <Accordion flush>
+      <AccordionItem eventKey="0">
+        <AccordionHeader>Rooms</AccordionHeader>
+        <AccordionBody>
+          <Accordion flush>
+            <AccordionItem eventKey="0">
+              <AccordionHeader>Vacant</AccordionHeader>
+              <AccordionBody>
+                <RoomsList rooms={rooms} status="Vacant" />
+              </AccordionBody>
+            </AccordionItem>
+            <AccordionItem eventKey="1">
+              <AccordionHeader>Occupied</AccordionHeader>
+              <AccordionBody>
+                <RoomsList rooms={rooms} status="Occupied" />
+              </AccordionBody>
+            </AccordionItem>
+            <AccordionItem eventKey="2">
+              <AccordionHeader>Out of Commission</AccordionHeader>
+              <AccordionBody>
+                <RoomsList rooms={rooms} status="Out of Commission" />
+              </AccordionBody>
+            </AccordionItem>
+          </Accordion>
+        </AccordionBody>
+      </AccordionItem>
+      <AccordionItem eventKey="1">
+        <AccordionHeader>Faculties</AccordionHeader>
+        <AccordionBody>
+          <Accordion flush>
+            <AccordionItem eventKey="0">
+              <AccordionHeader>Assigned</AccordionHeader>
+              <AccordionBody>Body</AccordionBody>
+            </AccordionItem>
+            <AccordionItem eventKey="1">
+              <AccordionHeader>Unassigned</AccordionHeader>
+              <AccordionBody>Body</AccordionBody>
+            </AccordionItem>
+          </Accordion>
+        </AccordionBody>
+      </AccordionItem>
+    </Accordion>
+  </>
+);
 
-  return (
-    <>
-      <h2 style={{ margin: '15px 0' }}>List View</h2>
-      <Accordion flush>
-        <AccordionItem eventKey="0">
-          <AccordionHeader>Rooms</AccordionHeader>
-          <AccordionBody>
-            <Accordion flush>
-              <AccordionItem eventKey="0">
-                <AccordionHeader>Vacant</AccordionHeader>
-                <AccordionBody>
-                  <VacantRooms />
-                </AccordionBody>
-              </AccordionItem>
-              <AccordionItem eventKey="1">
-                <AccordionHeader>Occupied</AccordionHeader>
-                <AccordionBody>
-                  <OccupiedRooms />
-                </AccordionBody>
-              </AccordionItem>
-              <AccordionItem eventKey="2">
-                <AccordionHeader>Out of Commission</AccordionHeader>
-                <AccordionBody>
-                  <OutOfCommissionRooms />
-                </AccordionBody>
-              </AccordionItem>
-            </Accordion>
-          </AccordionBody>
-        </AccordionItem>
-        <AccordionItem eventKey="1">
-          <AccordionHeader>Faculties</AccordionHeader>
-          <AccordionBody>
-            <Accordion flush>
-              <AccordionItem eventKey="0">
-                <AccordionHeader>Assigned</AccordionHeader>
-                <AccordionBody>Body</AccordionBody>
-              </AccordionItem>
-              <AccordionItem eventKey="1">
-                <AccordionHeader>Unassigned</AccordionHeader>
-                <AccordionBody>Body</AccordionBody>
-              </AccordionItem>
-            </Accordion>
-          </AccordionBody>
-        </AccordionItem>
-      </Accordion>
-    </>
-  );
+RoomsList.propTypes = {
+  rooms: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      roomNumber: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  status: PropTypes.string.isRequired,
 };
 
 ListView.propTypes = {
   rooms: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      _id: PropTypes.string.isRequired,
       roomNumber: PropTypes.string.isRequired,
       location: PropTypes.string.isRequired,
     }),
