9d0d5c7fa4bf13ffd73c902e685e98bbfa4df2c4
malialiu <mliu0141@gmail.com>
2023-04-02 03:40:28 -1000

    Implemented delete image #156

--- app/imports/ui/pages/EditFacultyProfile.jsx
+++ app/imports/ui/pages/EditFacultyProfile.jsx
@@ -48,7 +48,9 @@ const EditFacultyProfile = ({ id }) => {
     let imageUrl = initialImage;
     if (imageSubmit.current !== initialImage) {
       imageUrl = await uploadImgUrl(imageSubmit.current);
-      Meteor.call('deleteImage', doc.image);
+      if (doc.image.includes('cloudinary')) {
+        Meteor.call('deleteImage', doc.image);
+      }
     }
     const { firstName, lastName, bio, rooms, phoneNumber, officeHours } = data;
     const collectionName = FacultyProfiles.getCollectionName();
