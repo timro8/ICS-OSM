f788f380572bfa7be2ac3be64517fa2b1e297326
James Louie Grande <jlgrande@hawaii.edu>
2023-04-10 11:05:10 -1000

    Make progress bars functional

--- app/imports/ui/pages/home/progress-bar/ProgressBar.jsx
+++ app/imports/ui/pages/home/progress-bar/ProgressBar.jsx
@@ -1,17 +1,45 @@
 import { ProgressBar } from 'react-bootstrap';
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Rooms } from '../../../../api/room/RoomCollection';
+import { getRoomData } from '../../../../api/utilities/getRoomData';
 
-const getProgressBar = (title, number) => (
-  <div style={{ width: '25rem' }}>
-    {title}
-    <ProgressBar now={number} />
-  </div>
-);
+const ProgressBars = () => {
+  const { rooms } = useTracker(() => {
+    const subscription = Rooms.subscribeRoom();
+    return {
+      ready: subscription.ready(),
+      rooms: Rooms.find().fetch(),
+    };
+  });
 
-export const OccupiedProgressBar = () => getProgressBar('Rooms Occupied', 42);
+  const roomIds = rooms.map(room => room._id);
+  const roomsData = roomIds.map(room => getRoomData(room));
 
-export const VacantProgressBar = () => getProgressBar('Rooms Vacant', 9);
+  const amtVacant = roomsData.filter(room => room.status === 'Vacant').length;
+  const amtOutOfCommission = roomsData.filter(room => room.status === 'Out of Comission').length;
+  const amtOccupied = roomsData.filter(room => room.status === 'Occupied').length;
 
-export const OutOfCommissionProgressBar = () => getProgressBar('Rooms Out Of Commission', 3);
+  const totalRooms = roomsData.length;
 
-// TODO: replace the numbers with actual data
+  const getPercentage = (number, total) => (number / total) * 100;
+
+  return (
+    <>
+      <div style={{ width: '25rem' }}>
+        Vacant
+        <ProgressBar now={getPercentage(amtVacant, totalRooms)} label={amtVacant} />
+      </div>
+      <div style={{ width: '25rem' }}>
+        Occupied
+        <ProgressBar now={getPercentage(amtOccupied, totalRooms)} label={amtOccupied} />
+      </div>
+      <div style={{ width: '25rem' }}>
+        Out of commission
+        <ProgressBar now={getPercentage(amtOutOfCommission, totalRooms)} label={amtOutOfCommission} />
+      </div>
+    </>
+  );
+};
+
+export default ProgressBars;
