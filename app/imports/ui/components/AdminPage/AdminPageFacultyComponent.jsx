7a3518d4fc44f919364ac3d9da931ffd7c087593
JeromeWasserman <46459313+JeromeWasserman@users.noreply.github.com>
2023-04-11 20:17:32 -1000

    implemented delete

--- app/imports/ui/components/AdminPage/AdminPageFacultyComponent.jsx
+++ app/imports/ui/components/AdminPage/AdminPageFacultyComponent.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Image } from 'react-bootstrap';
+import { Image, Button } from 'react-bootstrap';
+import swal from 'sweetalert';
 import { COMPONENT_IDS } from '../../utilities/ComponentIDs';
+import { FacultyProfiles } from '../../../api/user/FacultyProfileCollection';
 import EditFacultyProfile from '../../pages/EditFacultyProfile';
+import { removeItMethod } from '../../../api/base/BaseCollection.methods';
+
 /** Renders a single row of Faculty members in a (Admin) table. See pages/AdminPageFacultyComponent.jsx. */
-const AdminPageFacultyComponent = ({ facultyProfile }) => (
-  <tr>
-    <td>
-      <Image roundedCircle src={facultyProfile.image} height="35rem" className="px-2" />
-      <Link className={COMPONENT_IDS.LIST_FACULTY_ADMIN} to={`/profile/${facultyProfile._id}`}>{facultyProfile.firstName} {facultyProfile.lastName}</Link>
-    </td>
-    <td>{facultyProfile.email}</td>
-    <td>{facultyProfile.facRole}</td>
-    <td>{facultyProfile.rooms}</td>
-    <td><EditFacultyProfile id={facultyProfile._id}> Edit</EditFacultyProfile></td>
-  </tr>
-);
+const AdminPageFacultyComponent = ({ facultyProfile }) => {
+
+  const deleteUser = () => {
+    const collectionName = FacultyProfiles.getCollectionName();
+    const roomId = facultyProfile._id;
+    removeItMethod.callPromise({ collectionName, instance: roomId })
+      .catch(error => swal('Error', error.message, 'error'))
+      .then(() => {
+        swal('Success', 'Room removed successfully', 'success');
+      });
+  };
+
+  return (
+    <tr>
+      <td>
+        <Image roundedCircle src={facultyProfile.image} height="35rem" className="px-2" />
+        <Link className={COMPONENT_IDS.LIST_FACULTY_ADMIN} to={`/profile/${facultyProfile._id}`}>{facultyProfile.firstName} {facultyProfile.lastName}</Link>
+      </td>
+      <td>{facultyProfile.email}</td>
+      <td>{facultyProfile.facRole}</td>
+      <td>{facultyProfile.rooms}</td>
+      <td><EditFacultyProfile id={facultyProfile._id}> Edit</EditFacultyProfile></td>
+      <Button className="btn btn-danger btn-delete d-inline" onClick={deleteUser}>Delete</Button>
+    </tr>
+  );
+};
 
 // Require a document to be passed to this component.
 AdminPageFacultyComponent.propTypes = {
