19709c0723fd959fc065ae970b0763f5fb4aabd9
susankpma <susanm@hawaii.edu>
2023-04-12 23:15:24 -1000

    implemented remove equipment and jack buttons

--- app/imports/ui/components/Tech/TechRoomEquipment.jsx
+++ app/imports/ui/components/Tech/TechRoomEquipment.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+import swal from 'sweetalert';
+import { RoomEquipments } from '../../../api/room/RoomEquipments';
 import EditEquipment from '../Editpages/EditEquipment';
+import { removeItMethod } from '../../../api/base/BaseCollection.methods';
 
 // Renders the jacks for the room. See pages/Tech.jsx. The EditJack component used for each jack based on jackId
-const TechRoomEquipment = ({ equipment }) => (
-  <tr>
-    <td>{equipment.roomNumber}</td>
-    <td>{equipment.quantity}</td>
-    <td>{equipment.description}</td>
-    <td>{equipment.serialNumber}</td>
-    <td>{equipment.assetTag}</td>
-    <td>{equipment.equipmentType}</td>
-    <td><EditEquipment equipmentId={equipment._id} /></td>
-  </tr>
-);
+const TechRoomEquipment = ({ equipment }) => {
+// delete the equipment
+  const deleteEquipment = () => {
+    const collectionName = RoomEquipments.getCollectionName();
+    removeItMethod.callPromise({ collectionName, instance: equipment._id })
+      .catch(error => swal('Error', error.message, 'error'))
+      .then(() => {
+        swal('Success', 'Equipment removed successfully', 'success');
+      });
+  };
+
+  return (
+    <tr>
+      <td>{equipment.roomNumber}</td>
+      <td>{equipment.quantity}</td>
+      <td>{equipment.description}</td>
+      <td>{equipment.serialNumber}</td>
+      <td>{equipment.assetTag}</td>
+      <td>{equipment.equipmentType}</td>
+      <td><EditEquipment equipmentId={equipment._id} /> <Button variant="outline-danger" size="sm" onClick={deleteEquipment}>Delete {equipment.description}</Button></td>
+    </tr>
+  );
+};
 
 // Requires a document to be passed to this component.
 TechRoomEquipment.propTypes = {
