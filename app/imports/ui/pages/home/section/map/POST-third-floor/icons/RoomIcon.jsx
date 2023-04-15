a2984df15ab25a9a5707910283c8601bad08ebe5
James Louie Grande <jlgrande@hawaii.edu>
2023-04-14 19:18:57 -1000

    Delete room icon modal

--- app/imports/ui/pages/home/section/map/POST-third-floor/icons/RoomIcon.jsx
+++ app/imports/ui/pages/home/section/map/POST-third-floor/icons/RoomIcon.jsx
@@ -1,14 +1,8 @@
-import { Button, Modal } from 'react-bootstrap';
-import React, { useState } from 'react';
 import { OverlayTrigger, Popover, PopoverBody } from 'react-bootstrap';
+import React from 'react';
 import roomPositions from '../../../../../../../api/room/RoomPositions';
 
 const RoomIcon = (props) => {
-
-  const [show, setShow] = useState(false);
-  const [roomIndex, setRoomIndex] = useState(null);
-  const handleClose = () => { setRoomIndex(null); setShow(false); };
-
   const getRoomColor = (status) => {
     if (status === 'Out of Commission') {
       return '#dc3545';
@@ -16,7 +10,6 @@ const RoomIcon = (props) => {
     return '#fff';
   };
 
-  return props.rooms.map((room, index) => {
   const roomPopover = room => (
     <Popover>
       <PopoverBody>
@@ -28,20 +21,16 @@ const RoomIcon = (props) => {
     </Popover>
   );
 
+  return props.rooms.map((room) => {
     const roomPosition = roomPositions.find(element => element.roomNumber === room.roomNumber);
     if (roomPosition) {
       const roomPositionTop = (roomPosition.top / 100) * props.mapHeight;
       const roomPositionLeft = (roomPosition.left / 100) * props.mapWidth;
       return (
-        <>
         <OverlayTrigger trigger={['hover', 'focus']} defaultShow={false} placement="bottom" overlay={roomPopover(room)}>
           <button
             type="button"
             className="map-icon map-icon-room"
-            onClick={() => {
-              setRoomIndex(index);
-              setShow(true);
-            }}
             style={{
               top: room.status === 'Occupied' ? `${roomPositionTop + 12}px` : `${roomPositionTop}px`,
               left: `${roomPositionLeft}px`,
@@ -50,27 +39,6 @@ const RoomIcon = (props) => {
           >
             {room.roomNumber}
           </button>
-          {roomIndex === index && (
-            <Modal show={show} onHide={handleClose} id={index}>
-              <Modal.Header closeButton>
-                <Modal.Title>Room {room.roomNumber} Details</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>Room info!{room.location}
-                <div>
-                  {room.status}
-                  {room.capacity}
-                  {room.roomSqFoot}
-                  {room.roomClassification}
-                </div>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          )}
-        </>
         </OverlayTrigger>
       );
     }
